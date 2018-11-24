cef7c85c2b4f6d3bda8d72e6cff32ce3e01023a5
jakub41 <lemiszewski@gmx.com>
2018-11-24 15:27:14 +0100

    Added models for Game & Match

---
+++ app/models/game.rb
@@ -0,0 +1,22 @@
+class Game
+  def self.start()
+
+  end
+
+  def self.withdraw()
+
+  end
+
+  def self.oppoent_for()
+
+  end
+
+  def self.take_turn()
+
+  end
+
+  def self.new_game()
+
+  end
+
+end
---
+++ app/models/match.rb
@@ -0,0 +1,14 @@
+class Match
+  def self.create()
+
+  end
+
+  def self.remove()
+
+  end
+
+  def self.clear_all
+
+  end
+
+end
